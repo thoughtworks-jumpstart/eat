f9e56ad40f3e65f68bbf7375680761b049dcb04c
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-09-17 17:01:52 +0800

    Add color and emoji to output

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
+const chalk = require("chalk");
 const cli = require("commander");
 const sample = require("lodash.sample");
 const restaurants = require("./restaurants.json");
 
 cli
-  .action(function() {
+  .action(() => {
     const restaurant = sample(restaurants);
-    console.log(`🍽 Let's go to ${restaurant.name} at ${restaurant.address}`);
+    const { name, address } = restaurant;
+    console.log(`🏃‍♀️  Let's go to: \n🍽   ${chalk.yellow(name)} \n📍  at ${chalk.magenta(address)}`);
   })
   .parse(process.argv);
