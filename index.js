10e48e60948e338f4cc0e9551dcddadf8f7edb25
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-09-17 12:29:31 +0800

    Move restaurants to its own file

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 #!/usr/bin/env node
 
-const sample = require("lodash.sample");
 const cli = require("commander");
-
-const restaurants = [
-  {
-    name: "Hong Lim Food Centre and Market",
-    address: "Blk 531A Upper Cross Street"
-  },
-  { name: "Chinatown Complex Market", address: "Blk 335 Smith Street" },
-  { name: "People's Park Food Centre", address: "Blk 32 New Market Road" },
-  {
-    name: "Market Street Interim Hawker Centre",
-    address: "5 Cross Street Market Street "
-  }
-];
+const sample = require("lodash.sample");
+const restaurants = require("./restaurants.json");
 
 cli
   .action(function() {
     const restaurant = sample(restaurants);
-    console.log(`Let's go to ${restaurant.name} at ${restaurant.address}`);
+    console.log(`🍽 Let's go to ${restaurant.name} at ${restaurant.address}`);
   })
   .parse(process.argv);
